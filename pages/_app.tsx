9fe79f82c859aee0de2af0a8257c9fae30a35258
qbentil <bentilshadrack72@gmail.com>
2023-01-10 00:17:53 +0000

    ✨: Configure Redux in root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import { StateProvider } from '../context/StateProvider'
+import { initialState } from '../context/initialState'
+import reducer from '../context/reducer'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Component {...pageProps} />
+    </StateProvider>
+  )
 }
