d049603b28f9cedc1195c2e46f8271895bd0f805
qbentil <bentilshadrack72@gmail.com>
2023-01-10 01:22:33 +0000

    🚀Setup primary coler

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6941C6"
+      },
+    },
     display: ["group-hover"],
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
