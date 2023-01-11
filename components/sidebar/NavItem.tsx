58af126c38069034d05f9c77f446ec7427af4872
qbentil <bentilshadrack72@gmail.com>
2023-01-11 02:18:15 +0000

    🚀Nav Items component

---
+++ components/sidebar/NavItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavItem } from "../../types";
+import Link from "next/link";
+
+const NavItem = ({
+  data,
+  onClick,
+}: {
+  data: NavItem;
+  onClick?: () => void;
+}) => {
+  const { name, link, icon } = data;
+  return (
+    <Link
+      href={link || "#"}
+      onClick={onClick}
+      className="flex items-center gap-x-4 p-2 w-full text-base rounded-lg text-white hover:bg-hover"
+    >
+      <span className="text-xl">{icon}</span>
+      <span className="">{name}</span>
+    </Link>
+  );
+};
+
+export default NavItem;
