b669047807a43575fb6c31028b161fdb222846a5
qbentil <bentilshadrack72@gmail.com>
2023-01-13 15:05:24 +0000

    🚀Create Axios base config

---
+++ utils/Axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const Axios = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
+});
+
+export default Axios;
