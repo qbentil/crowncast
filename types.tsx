b2abd696728265eea7329ea5dedf3755b5817ac2
qbentil <bentilshadrack72@gmail.com>
2023-01-13 11:06:57 +0000

    🚀Create User and Company types export

--- types.tsx
+++ types.tsx
@@ -4,3 +4,25 @@ export type NavItem = {
     icon: JSX.Element | string;
     protected: boolean;
 }
+
+export type User = {
+    _id: string,
+    name: string,
+    email: string,
+    company?: Company,
+    phone?: string,
+    token?: string,
+    role?: string,
+    createdAt?: string,
+    updatedAt?: string,
+    avatar?: string,
+    address?: string,
+    status: "active" | "inactive"
+}
+
+export type Company = {
+    _id: string,
+    name: string,
+    address: string,
+    phone?: string,
+}
