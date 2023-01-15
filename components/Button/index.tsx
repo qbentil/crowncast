4c1221734fe1a2a75a6cd5dd021520d2e14680dc
qbentil <bentilshadrack72@gmail.com>
2023-01-15 16:48:36 +0000

    🚀Create custom Button Component

---
+++ components/Button/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+interface Props {
+    onClick?: () => void
+    className?: string
+    type?: 'button' | 'submit' | 'reset'
+    disabled?: boolean
+    loading?: boolean
+    variant?: 'primary' | 'secondary' | 'danger' | 'success' | 'warning'
+    size?: 'sm' | 'md' | 'lg'
+    block?: boolean
+    icon?: React.ReactNode
+    iconPosition?: 'left' | 'right'
+    iconOnly?: boolean
+    title?: string
+}
+
+const Button = ({className, icon, iconPosition = "left", loading,onClick, disabled, title, variant }: Props) => {
+  return (
+    <button className={` py-2 px-4 rounded flex items-center justify-center gap-x-2 ${className} ${
+        variant === 'secondary' ? 'bg-white text-primary border border-gray-300' :
+        variant === 'danger' ? 'bg-red-600 text-white' :
+        variant === 'success' ? 'bg-green-600 text-white' :
+        variant === 'warning' ? 'bg-yellow-600 text-white' : 'bg-primary text-white'
+
+    }`}>
+        {icon && iconPosition === 'left' && icon}
+        {loading ? 'Loading...' : title}
+        {icon && iconPosition === 'right' && icon}
+        
+    </button>
+  )
+}
+
+export default Button
