1b8043f596069b7b9734f050f5f567ba99767ab2
qbentil <bentilshadrack72@gmail.com>
2023-01-15 16:50:00 +0000

    Update Content Area

--- components/content/index.tsx
+++ components/content/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import InfoGraphics from "../infographic";
+import PageTop from "./navbar";
+import React from "react";
 
 const ContentArea = () => {
   return (
-    <div className="w-full h-full bg-gray-200 flex flex-col gap-y-4 items-center justify-center">
-    <h1 className="text-2xl font-bold text-gray-700">Dashboard</h1>
-</div>
-  )
-}
+    <div className="w-full h-full flex flex-col gap-y-4 px-6 py-4">
+      <PageTop />
+      <InfoGraphics />
+    </div>
+  );
+};
 
-export default ContentArea
+export default ContentArea;
