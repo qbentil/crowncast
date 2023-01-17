69d913bad24b8d60ce30fbf4b85e508543a2cb4e
qbentil <bentilshadrack72@gmail.com>
2023-01-17 18:07:21 +0000

    Create dump util datat

---
+++ utils/dump.tsx
@@ -0,0 +1,23 @@
+export const Categories = [
+    {
+        _id: "1",
+        title: "Art",
+        description: "Art is a diverse range of human activities in creating visual, auditory or performing artifacts (artworks), expressing the author's imaginative or technical skill, intended to be appreciated for their beauty or emotional power. In their most general form these activities include the production of works of art, the criticism of art, the study of the history of art, and the aesthetic dissemination of art.",
+        createdAt: "2021-05-01T12:00:00.000Z",
+        updatedAt: "2021-05-01T12:00:00.000Z",
+    },
+    {
+        _id: "2",
+        title: "Music",
+        description: "Music is an art form and cultural activity whose medium is sound organized in time. General definitions of music include common elements such as pitch (which governs melody and harmony), rhythm (and its associated concepts tempo, meter, and articulation), dynamics, and the sonic qualities of timbre and texture. Different styles or types of music may emphasize, de-emphasize or omit some of these elements. Music is performed with a vast range of instruments and vocal techniques ranging from singing to rapping; there are solely instrumental pieces, solely vocal pieces (such as songs without instrumental accompaniment) and pieces that combine singing and instruments. The word derives from Greek μουσική (mousike; \"art of the Muses\").",
+        createdAt: "2021-05-01T12:00:00.000Z",
+        updatedAt: "2021-05-01T12:00:00.000Z",
+    },
+    {
+        _id: "3",
+        title: "Dance",
+        description: "Dance is a performing art form consisting of purposefully selected sequences of human movement. This movement has aesthetic and symbolic value, and is acknowledged as dance by performers and observers within a particular culture. Dance can be categorized and described by its choreography, by its repertoire of movements, or by its historical period or place of origin.",
+        createdAt: "2021-05-01T12:00:00.000Z",
+        updatedAt: "2021-05-01T12:00:00.000Z",
+    }
+]
