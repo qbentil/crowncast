eee778c3bc0ad788320cda65af8a068af640b370
qbentil <bentilshadrack72@gmail.com>
2023-01-17 18:07:34 +0000

    Update contestans sole data

--- components/contestants/index.tsx
+++ components/contestants/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Filter from "../filter";
+import { Categories } from "../../utils/dump";
 
 const Contestants = () => {
   return (
-    <div>Contestants</div>
-  )
-}
+    <div className="w-full">
+      {/* header */}
+      <div className="flex items-center justify-between">
+        <Filter data={Categories} onChange={() => null} />
+      </div>
+    </div>
+  );
+};
 
-export default Contestants
+export default Contestants;
