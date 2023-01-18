147c939ca894892fbe004ab128403621ad35ea44
qbentil <bentilshadrack72@gmail.com>
2023-01-18 23:41:30 +0000

    Handle filter events🚀🚀🚀

--- components/contestants/index.tsx
+++ components/contestants/index.tsx
@@ -1,26 +1,58 @@
-import React from "react";
-import { Categories } from "../../utils/dump";
 import { AiOutlineClose } from "react-icons/ai";
-import Searchbar from "../searchbar";
 import Filter from "../filter";
+import React from "react";
+import Searchbar from "../searchbar";
+import { useState } from "react";
+import { OptionType } from "../../types";
+const filters = [
+  {
+    value : "votes",
+    label: "Votes"
+  },
+  {
+    value: "events",
+    label: "Events"
+  },
+  {
+    value: "categories",
+    label: "Categories"
+  }
+]
 
 const Contestants = () => {
+  const [filterby, setFilterby] = useState<OptionType[] | []>([])
+  const [data, setData] = useState<OptionType[] | []>(filters)
+  const filterChange = (option: OptionType) => {
+    // check if option is already in filterby
+    const isOptionInFilter = filterby.find((item: OptionType) => item.value === option.value);
+    if(isOptionInFilter) {
+      const newFilter = filterby.filter((item: OptionType) => item.value !== option.value);
+      setFilterby(newFilter);
+
+    } else {
+      setFilterby([...filterby, option]);
+    }
+  }
+  const removeSelect = (option: OptionType) => {
+    const newFilter = filterby.filter((item: OptionType) => item.value !== option.value);
+    setFilterby(newFilter);
+  }
   return (
     <div className="w-full">
       {/* header */}
       <div className="flex items-center justify-between">
         <div className="flex items-center justify-start gap-x-8 min-w-[25rem]">
-          <Filter data={Categories} onChange={() => null} />
+          <Filter options={data} selected={filterby} onChange={filterChange} />
           {/* filters */}
           <div className="flex items-center gap-x-4">
-            <div className="flex flex-row-reverse items-center justify-center gap-x-2 bg-primary-50 text-primary-700 rounded px-4 py-1 text-sm shadow-sm cursor-pointer">
-              <AiOutlineClose />
-              <span>Event</span>
-            </div>
-            <div className="flex flex-row-reverse items-center justify-center gap-x-2 bg-primary-50 text-primary-700 rounded px-4 py-1 text-sm shadow-sm cursor-pointer">
-              <AiOutlineClose />
-              <span>Categories</span>
-            </div>
+              {
+                filterby.length > 0 && filterby.map((option: OptionType) => (
+                  <div key={option.value} className="flex flex-row-reverse items-center justify-center gap-x-2 bg-primary-50 text-primary-700 rounded px-4 py-1 text-sm shadow-sm cursor-pointer">
+                  <AiOutlineClose onClick={() => removeSelect(option)} />
+                  <span>{option.label}</span>
+                </div>
+                ))
+              }
           </div>
         </div>
 
