351d6706c60fa51d5e8ef32a2fc033156f4d352d
qbentil <bentilshadrack72@gmail.com>
2023-01-27 10:21:58 +0000

    Setup contestants sub page

---
+++ pages/dashboard/contestants/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import React from "react";
+import { ContentArea, Login, Sidebar } from "../../../components";
+import { useStateValue } from "../../../context/StateProvider";
+
+const ContestantsPage = () => {
+  const [{ user }, dispatch] = useStateValue();
+  if (!user) return <Login />;
+  return (
+    <>
+      <Head>
+        <title>Contestant | Administrator</title>
+        <meta name="description" content="CrownCast Portal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={"w-full h-[100vh] flex items-center justify-center"}>
+        {/* 25% width for sidebar */}
+        <Sidebar />
+        {/* 75% width for content */}
+        <ContentArea />
+      </main>
+    </>
+  );
+};
+
+export default ContestantsPage;
