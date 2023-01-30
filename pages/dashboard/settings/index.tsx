544c7151e7894bc46f422066455f89e0020da1c2
qbentil <bentilshadrack72@gmail.com>
2023-01-30 11:42:05 +0000

    Setup Settings sub pages

---
+++ pages/dashboard/settings/index.tsx
@@ -0,0 +1,30 @@
+import { ContentArea, Login, Sidebar } from "../../../components";
+
+import Head from "next/head";
+import React from "react";
+import { useStateValue } from "../../../context/StateProvider";
+
+const SettingsPage = () => {
+  const [{ user }, dispatch] = useStateValue();
+  if (!user) return <Login />;
+  return (
+    <>
+      <Head>
+        <title>Contestant | Administrator</title>
+        <meta name="description" content="CrownCast Portal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={"w-full h-[100vh] flex items-center justify-center"}>
+        {/* 25% width for sidebar */}
+        <Sidebar />
+        {/* 75% width for content */}
+        <ContentArea>
+          <p>System Settings</p>
+        </ContentArea>
+      </main>
+    </>
+  );
+};
+
+export default SettingsPage;
