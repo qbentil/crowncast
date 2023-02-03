40675f3c3c876a70ab0069c6192dc2e113f1ae9b
qbentil <bentilshadrack72@gmail.com>
2023-02-03 15:38:41 +0000

    Create new filter optioons for events

--- components/events/index.tsx
+++ components/events/index.tsx
@@ -1,7 +1,27 @@
-import { Filters } from "../../utils/dump";
+import { OptionType } from '../../types'
+import React from "react";
 import SubHeader from "../subpage-header";
 import Table from "./table";
-import React from "react";
+
+const Filters = [
+  {
+    value: "Opening Date",
+    label: "Opening Date",
+  },
+  {
+    value: "Organizer",
+    label: "Organizer",
+  },
+  {
+    value: "Cost /vote",
+    label: "Cost /vote",
+  },
+  {
+    value: "Category",
+    label: "Category",
+  }
+] as OptionType[]
+
 
 const Events = () => {
 
