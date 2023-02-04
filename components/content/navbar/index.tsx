79061ec977024362669fc135e63807af8f47e442
qbentil <bentilshadrack72@gmail.com>
2023-02-04 00:20:59 +0000

    Update Navbar

--- components/content/navbar/index.tsx
+++ components/content/navbar/index.tsx
@@ -1,8 +1,8 @@
+import { BiPlus } from "react-icons/bi";
 import Button from "../../Button";
 import { FiUploadCloud } from "react-icons/fi";
 import React from "react";
 import { useStateValue } from "../../../context/StateProvider";
-import { BiPlus } from "react-icons/bi";
 
 const PageTop = () => {
   const [{ user }, dispatch] = useStateValue();
@@ -10,7 +10,7 @@ const PageTop = () => {
     <nav className="w-full flex items-center justify-between">
       <div className="">
         <h1 className="text-balck text-xl">
-          Welcome back, <span className="font-bold">{user?.name || ""}</span>
+          Welcome back, <span className="font-bold">{user?.name.split(" ")[user?.name.split(" ").length - 1] || ""}</span>
         </h1>
         <p className="text-sm text-gray-500">Track, manage and forecast your pageantry competition.</p>
       </div>
