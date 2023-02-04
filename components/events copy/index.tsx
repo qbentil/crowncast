75850b0b394eea47c45601bb542d5d7a4cd92611
qbentil <bentilshadrack72@gmail.com>
2023-02-04 00:21:13 +0000

    Setup Organizers base component

---
+++ components/events copy/index.tsx
@@ -0,0 +1,35 @@
+import { OptionType } from '../../types'
+import React from "react";
+import SubHeader from "../subpage-header";
+import Table from "./table";
+
+const Filters = [
+  {
+    value: "Company",
+    label: "Company",
+  },
+  {
+    value: "Name",
+    label: "Name",
+  },
+  {
+    value: "Years",
+    label: "Date Joined",
+  }
+] as OptionType[]
+
+
+const Organizers = () => {
+
+  return (
+    <div className="w-full">
+      {/* header */}
+      <SubHeader title="Organizers" filters={Filters} />
+
+      {/* table */}
+      <Table />
+    </div>
+  );
+};
+
+export default Organizers;
