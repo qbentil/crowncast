9d7cf0505eec940fe54bface6e962cb62f520f39
qbentil <bentilshadrack72@gmail.com>
2023-02-13 23:49:53 +0000

    Init categories  page base

---
+++ components/categories/index.tsx
@@ -0,0 +1,39 @@
+import { OptionType } from '../../types'
+import React from "react";
+import SubHeader from "../subpage-header";
+import Table from "./table";
+
+const Filters = [
+  {
+    value: "Opening Date",
+    label: "Opening Date",
+  },
+  {
+    value: "Organizer",
+    label: "Organizer",
+  },
+  {
+    value: "Cost /vote",
+    label: "Cost /vote",
+  },
+  {
+    value: "Category",
+    label: "Category",
+  }
+] as OptionType[]
+
+
+const Events = () => {
+
+  return (
+    <div className="w-full">
+      {/* header */}
+      <SubHeader title="Events" filters={Filters} />
+
+      {/* table */}
+      <Table />
+    </div>
+  );
+};
+
+export default Events;
