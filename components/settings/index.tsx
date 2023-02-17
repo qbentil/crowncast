7d703d694edd19fecc8c9e0c64d3cf0f9ee89b85
qbentil <bentilshadrack72@gmail.com>
2023-02-17 13:19:32 +0000

    Setup Settings Base admin component

---
+++ components/settings/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div>
+        <p>
+            Settings Page Content Here ...  
+        </p>
+    </div>
+  )
+}
+
+export default Settings
