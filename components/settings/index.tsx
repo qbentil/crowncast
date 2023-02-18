c9b1f42fdcc6bf31dfdb379a6b734d27409584f3
qbentil <bentilshadrack72@gmail.com>
2023-02-18 23:13:46 +0000

    Implementing the settings sub page

--- components/settings/index.tsx
+++ components/settings/index.tsx
@@ -1,13 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const Settings = () => {
-  return (
-    <div>
-        <p>
-            Settings Page Content Here ...  
-        </p>
-    </div>
-  )
+    return (
+        <div>
+            {/*  */}
+            <div>
+                <h2>Personal Information</h2>
+                <p>Update your photo and Personal details here!</p>
+            </div>
+            {/* image and uploader */}
+            <div>
+                <div className="w-60">
+                    <img src="/assets/bentil.jpeg" alt="current" className=' w-full rounded' />
+                </div>
+                <div>
+                    <button>Upload</button>
+                    <button>Remove</button>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Settings
