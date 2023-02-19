8bae3e5b77c9397eb1db186025597b1adf34d41d
qbentil <bentilshadrack72@gmail.com>
2023-02-19 18:13:37 +0000

    Configure dependencies in App entry point: toastify and motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,17 @@ import { StateProvider } from '../context/StateProvider'
 import { initialState } from '../context/initialState'
 import reducer from '../context/reducer'
 import '../styles/globals.css'
+import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from 'next/app'
-
+import { ToastContainer } from 'react-toastify';
+import { AnimatePresence } from 'framer-motion'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
-      <Component {...pageProps} />
+      <AnimatePresence>
+        <ToastContainer position='top-center' />
+        <Component {...pageProps} />
+      </AnimatePresence>
     </StateProvider>
   )
 }
