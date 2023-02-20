7826a6f3b307db3fdd6303a3f9952506064b5c58
qbentil <bentilshadrack72@gmail.com>
2023-02-20 11:17:33 +0000

    ConvertImageBox component base to TS

--- components/ImageUploader/imageBox.tsx
+++ components/ImageUploader/imageBox.tsx
@@ -3,16 +3,11 @@ import React from 'react'
 import { MdDeleteForever } from 'react-icons/md';
 
 interface Props {
-  imageURI: string;
-  setImage: any;
-  setImageURI: any;
+  setImage?: React.Dispatch<React.SetStateAction<string | null | undefined>>
+  imageURI?: string | undefined
 }
 
-const ImageBox = ({setImage, imageURI, setImageURI}: Props) => {
-  const clearImage = () => {
-    setImage(null);
-    setImageURI(null);
-  }
+const ImageBox = ({setImage, imageURI}: Props) => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center rounded-lg cursor-pointer border-2 border-dashed p-2 relative">
         <img
@@ -20,7 +15,7 @@ const ImageBox = ({setImage, imageURI, setImageURI}: Props) => {
           className="w-full h-full object-contain"
           alt='bentil'
         />
-      <div onClick={clearImage} className="w-10 h-10 flex items-center justify-center bg-red-500 hover:bg-red-600 rounded-full text-white absolute bottom-5 right-5">
+      <div onClick={()=> setImage && setImage(undefined)} className="w-10 h-10 flex items-center justify-center bg-red-500 hover:bg-red-600 rounded-full text-white absolute bottom-5 right-5">
         <MdDeleteForever />
       </div>
     </div>
