92bdc436b5dc0e076a3c013ad9f9285b1c04a2e3
qbentil <bentilshadrack72@gmail.com>
2023-02-20 11:17:16 +0000

    Convert Image uploader base to TS

--- components/ImageUploader/index.tsx
+++ components/ImageUploader/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ImageBox from "./imageBox";
 import Uploader from "./uploader";
+import { Interface } from "readline";
 
 interface Props {
-  image?: string;
-  setImage?: any;
-  setImageURI?: any;
-  className?: string;
+  image?: string | null,
+  setImage?: React.Dispatch<React.SetStateAction<string | null | undefined>>,
+  setImageURI?: React.Dispatch<React.SetStateAction<string | null | undefined>>,
+  className?: string  
 }
 
-const ImageUploader = ({image, setImage, setImageURI, className}: Props) => {
+const ImageUploader = ({image, setImage, setImageURI, className}:Props) => {
   return (
     <div className={className}>
       {image ? (
-        <ImageBox setImage={setImage} imageURI={image} setImageURI={setImageURI} />
+        <ImageBox setImage={setImage} imageURI={image} />
       ) : (
         <Uploader setImageURI={setImageURI} setImage={setImage} />
       )}
